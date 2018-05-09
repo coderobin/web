b83e854c5b3672faa13e8f36598ab8f2f70647ca
Dong Xie <xied75@gmail.com>
2018-05-09 16:44:47 +0100

    1, add contact page 2, remove old slider1.jpg, slider2,jpg 3, fill in lat,long of office location 4, add default.conf for Dockerfile for nginx config

--- app/scripts/custom.js
+++ app/scripts/custom.js
@@ -1,5 +1,5 @@
 
-jQuery(function($) {
+jQuery(function ($) {
     'use strict';
     /*============================================
 	Page Preloader
@@ -17,31 +17,30 @@ jQuery(function($) {
         });
     });
     //menu resizing on hover
-    if ($(window).scrollTop()< ($(window).height()-600)){
+    if ($(window).scrollTop() < ($(window).height() - 600)) {
         $('#main-navigation').removeClass('scrolled');
-        $('.main-logo img').attr('src','images/logo.png');
+        $('.main-logo img').attr('src', 'images/logo.png');
     }
-    else{
-        $('#main-navigation').addClass('scrolled'); 
-        $('.main-logo img').attr('src','images/logo-alt.png');
+    else {
+        $('#main-navigation').addClass('scrolled');
+        $('.main-logo img').attr('src', 'images/logo-alt.png');
     }
 
     $(window).on('scroll', function () {
-        if ($(window).scrollTop()< ($(window).height()-600)){
+        if ($(window).scrollTop() < ($(window).height() - 600)) {
             $('#main-navigation').removeClass('scrolled');
-            $('.main-logo img').attr('src','images/logo.png');
+            $('.main-logo img').attr('src', 'images/logo.png');
         }
-        else{
-            $('#main-navigation').addClass('scrolled'); 
-            $('.main-logo img').attr('src','images/logo-alt.png');
+        else {
+            $('#main-navigation').addClass('scrolled');
+            $('.main-logo img').attr('src', 'images/logo-alt.png');
         }
     });
-    
+
     /*============================================
 	Video Player
      ==============================================*/
-    if ($('.virtual-video').length)
-    {
+    if ($('.virtual-video').length) {
         $('.virtual-video').mediaelementplayer({
             alwaysShowControls: false
         }
@@ -51,40 +50,37 @@ jQuery(function($) {
     /*============================================
 	Video Background
      ==============================================*/
-    if ($('.video-background').length)
-    {
-					   
+    if ($('.video-background').length) {
+
         $('.video-background').videoBG({
-            mp4:'assets/videobg/assets/laptop-video.mp4',
-            ogv:'assets/videobg/assets/laptop-video.ogv',
-            webm:'assets/videobg/assets/laptop-video.webm',
-            poster:'assets/videobg/assets/laptop-video.png',
-            scale:true,
-            zIndex:0
+            mp4: 'assets/videobg/assets/laptop-video.mp4',
+            ogv: 'assets/videobg/assets/laptop-video.ogv',
+            webm: 'assets/videobg/assets/laptop-video.webm',
+            poster: 'assets/videobg/assets/laptop-video.png',
+            scale: true,
+            zIndex: 0
         });
     }
-    
-    
+
+
     /*============================================
 	Testimonials Carousel
      ==============================================*/
-    if ($('#testimonials-carousel').length)
-    {
+    if ($('#testimonials-carousel').length) {
         $('#testimonials-carousel').owlCarousel({
             autoPlay: 5000, //Set AutoPlay to 3 seconds
-            paginationSpeed : 800,
-            slideSpeed : 200,
-            rewindSpeed : 1000,
-            items : 3,
-            itemsDesktop : [1199,3],
-            itemsDesktopSmall : [979,2]
+            paginationSpeed: 800,
+            slideSpeed: 200,
+            rewindSpeed: 1000,
+            items: 3,
+            itemsDesktop: [1199, 3],
+            itemsDesktopSmall: [979, 2]
         });
     }
     /*============================================
 	Testimonials Carousel
      ==============================================*/
-    if ($('#about-carousel').length)
-    {
+    if ($('#about-carousel').length) {
         $('#about-carousel').owlCarousel({
             navigation: false, // Show next and prev buttons
             slideSpeed: 300,
@@ -96,8 +92,7 @@ jQuery(function($) {
     /*============================================
 	Text Rotator
      ==============================================*/
-    if ($('.rotate').length)
-    {
+    if ($('.rotate').length) {
         $('.rotate').textrotator({
             animation: 'flipUp', // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
             separator: ',', // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
@@ -107,19 +102,18 @@ jQuery(function($) {
     /*============================================
 	Flex Slider
      ==============================================*/
-    
-    if ($('.flexslider').length)
-    {
+
+    if ($('.flexslider').length) {
         $('.flexslider').flexslider({
             animation: 'slide',
             controlNav: false,
-            slideshowSpeed:5000
-        }); 
-        $('.flex-next').on('click', function(e){ 
+            slideshowSpeed: 5000
+        });
+        $('.flex-next').on('click', function (e) {
             e.preventDefault();
             $('.flexslider').flexslider('play')
         });
-        $('.flex-next').on('click', function(e){ 
+        $('.flex-next').on('click', function (e) {
             e.preventDefault();
             $('.flex-prev').flexslider('play')
         });
@@ -127,8 +121,7 @@ jQuery(function($) {
     /*============================================
 	Counter
      ==============================================*/
-    if ($('.count').length)
-    {
+    if ($('.count').length) {
         $('.count').counterUp({
             delay: 10,
             time: 1000
@@ -139,18 +132,18 @@ jQuery(function($) {
      ==============================================*/
     function toggleIcon(e) {
         $(e.target)
-        .prev('.panel-heading')
-        .find('.more-less')
-        .toggleClass('glyphicon-plus glyphicon-minus');
+            .prev('.panel-heading')
+            .find('.more-less')
+            .toggleClass('glyphicon-plus glyphicon-minus');
     }
     $('.panel-group').on('hidden.bs.collapse', toggleIcon);
     $('.panel-group').on('shown.bs.collapse', toggleIcon);
-    
+
     /*============================================
 	FAQ
      ==============================================*/
-    
-    $('#faq-categories a').on('click', function(event) {
+
+    $('#faq-categories a').on('click', function (event) {
         event.preventDefault();
 
         $('html, body').animate({
--- app/scripts/map.js
+++ app/scripts/map.js
@@ -1,84 +1,84 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
 	//set your google maps parameters
-	var latitude = 51.5255069,
-		longitude = -0.0836207,
+	var latitude = 51.748832,
+		longitude = -1.277470,
 		map_zoom = 14;
 
 	//google map custom marker icon - .png fallback for IE11
-	var is_internetExplorer11= navigator.userAgent.toLowerCase().indexOf('trident') > -1;
-	var marker_url = ( is_internetExplorer11 ) ? 'images/cd-icon-location.png' : 'images/cd-icon-location.svg';
-		
+	var is_internetExplorer11 = navigator.userAgent.toLowerCase().indexOf('trident') > -1;
+	var marker_url = (is_internetExplorer11) ? 'images/cd-icon-location.png' : 'images/cd-icon-location.svg';
+
 	//define the basic color of your map, plus a value for saturation and brightness
-	var	main_color = '#2d313f',
-		saturation_value= -20,
-		brightness_value= 5;
+	var main_color = '#2d313f',
+		saturation_value = -20,
+		brightness_value = 5;
 
 	//we define here the style of the map
-	var style= [ 
+	var style = [
 		{
 			//set saturation for the labels on the map
 			elementType: 'labels',
 			stylers: [
-				{saturation: saturation_value}
+				{ saturation: saturation_value }
 			]
-		},  
-	    {	//poi stands for point of interest - don't show these lables on the map 
+		},
+		{	//poi stands for point of interest - don't show these lables on the map 
 			featureType: 'poi',
 			elementType: 'labels',
 			stylers: [
-				{visibility: 'off'}
+				{ visibility: 'off' }
 			]
 		},
 		{
 			//don't show highways lables on the map
-	        featureType: 'road.highway',
-	        elementType: 'labels',
-	        stylers: [
-	            {visibility: 'off'}
-	        ]
-	    }, 
-		{ 	
-			//don't show local road lables on the map
-			featureType: 'road.local', 
-			elementType: 'labels.icon', 
+			featureType: 'road.highway',
+			elementType: 'labels',
 			stylers: [
-				{visibility: 'off'} 
-			] 
+				{ visibility: 'off' }
+			]
 		},
-		{ 
-			//don't show arterial road lables on the map
-			featureType: 'road.arterial', 
-			elementType: 'labels.icon', 
+		{
+			//don't show local road lables on the map
+			featureType: 'road.local',
+			elementType: 'labels.icon',
 			stylers: [
-				{visibility: 'off'}
-			] 
+				{ visibility: 'off' }
+			]
+		},
+		{
+			//don't show arterial road lables on the map
+			featureType: 'road.arterial',
+			elementType: 'labels.icon',
+			stylers: [
+				{ visibility: 'off' }
+			]
 		},
 		{
 			//don't show road lables on the map
 			featureType: 'road',
 			elementType: 'geometry.stroke',
 			stylers: [
-				{visibility: 'off'}
+				{ visibility: 'off' }
 			]
-		}, 
+		},
 		//style different elements on the map
-		{ 
-			featureType: 'transit', 
-			elementType: 'geometry.fill', 
+		{
+			featureType: 'transit',
+			elementType: 'geometry.fill',
 			stylers: [
 				{ hue: main_color },
-				{ visibility: 'on' }, 
-				{ lightness: brightness_value }, 
+				{ visibility: 'on' },
+				{ lightness: brightness_value },
 				{ saturation: saturation_value }
 			]
-		}, 
+		},
 		{
 			featureType: 'poi',
 			elementType: 'geometry.fill',
 			stylers: [
 				{ hue: main_color },
-				{ visibility: 'on' }, 
-				{ lightness: brightness_value }, 
+				{ visibility: 'on' },
+				{ lightness: brightness_value },
 				{ saturation: saturation_value }
 			]
 		},
@@ -87,8 +87,8 @@ jQuery(document).ready(function($){
 			elementType: 'geometry.fill',
 			stylers: [
 				{ hue: main_color },
-				{ visibility: 'on' }, 
-				{ lightness: brightness_value }, 
+				{ visibility: 'on' },
+				{ lightness: brightness_value },
 				{ saturation: saturation_value }
 			]
 		},
@@ -97,8 +97,8 @@ jQuery(document).ready(function($){
 			elementType: 'geometry.fill',
 			stylers: [
 				{ hue: main_color },
-				{ visibility: 'on' }, 
-				{ lightness: brightness_value }, 
+				{ visibility: 'on' },
+				{ lightness: brightness_value },
 				{ saturation: saturation_value }
 			]
 		},
@@ -107,8 +107,8 @@ jQuery(document).ready(function($){
 			elementType: 'geometry.fill',
 			stylers: [
 				{ hue: main_color },
-				{ visibility: 'on' }, 
-				{ lightness: brightness_value }, 
+				{ visibility: 'on' },
+				{ lightness: brightness_value },
 				{ saturation: saturation_value }
 			]
 		},
@@ -117,8 +117,8 @@ jQuery(document).ready(function($){
 			elementType: 'geometry.fill',
 			stylers: [
 				{ hue: main_color },
-				{ visibility: 'on' }, 
-				{ lightness: brightness_value }, 
+				{ visibility: 'on' },
+				{ lightness: brightness_value },
 				{ saturation: saturation_value }
 			]
 		},
@@ -127,8 +127,8 @@ jQuery(document).ready(function($){
 			elementType: 'geometry.fill',
 			stylers: [
 				{ hue: main_color },
-				{ visibility: 'on' }, 
-				{ lightness: brightness_value }, 
+				{ visibility: 'on' },
+				{ lightness: brightness_value },
 				{ saturation: saturation_value }
 			]
 		},
@@ -137,8 +137,8 @@ jQuery(document).ready(function($){
 			elementType: 'geometry.fill',
 			stylers: [
 				{ hue: main_color },
-				{ visibility: 'on' }, 
-				{ lightness: brightness_value }, 
+				{ visibility: 'on' },
+				{ lightness: brightness_value },
 				{ saturation: saturation_value }
 			]
 		},
@@ -146,19 +146,19 @@ jQuery(document).ready(function($){
 			featureType: 'landscape',
 			stylers: [
 				{ hue: main_color },
-				{ visibility: 'on' }, 
-				{ lightness: brightness_value }, 
+				{ visibility: 'on' },
+				{ lightness: brightness_value },
 				{ saturation: saturation_value }
 			]
-			
+
 		},
 		{
 			featureType: 'road',
 			elementType: 'geometry.fill',
 			stylers: [
 				{ hue: main_color },
-				{ visibility: 'on' }, 
-				{ lightness: brightness_value }, 
+				{ visibility: 'on' },
+				{ lightness: brightness_value },
 				{ saturation: saturation_value }
 			]
 		},
@@ -167,67 +167,65 @@ jQuery(document).ready(function($){
 			elementType: 'geometry.fill',
 			stylers: [
 				{ hue: main_color },
-				{ visibility: 'on' }, 
-				{ lightness: brightness_value }, 
+				{ visibility: 'on' },
+				{ lightness: brightness_value },
 				{ saturation: saturation_value }
 			]
-		}, 
+		},
 		{
 			featureType: 'water',
 			elementType: 'geometry',
 			stylers: [
 				{ hue: main_color },
-				{ visibility: 'on' }, 
-				{ lightness: brightness_value }, 
+				{ visibility: 'on' },
+				{ lightness: brightness_value },
 				{ saturation: saturation_value }
 			]
 		}
 	];
-		
+
 	//set google map options
 	var map_options = {
-      	center: new google.maps.LatLng(latitude, longitude),
-      	zoom: map_zoom,
-      	panControl: false,
-      	zoomControl: false,
-      	mapTypeControl: false,
-      	streetViewControl: false,
-      	mapTypeId: google.maps.MapTypeId.ROADMAP,
-      	scrollwheel: false,
-      	styles: style,
-    }
-    //inizialize the map
+		center: new google.maps.LatLng(latitude, longitude),
+		zoom: map_zoom,
+		panControl: false,
+		zoomControl: false,
+		mapTypeControl: false,
+		streetViewControl: false,
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		scrollwheel: false,
+		styles: style,
+	}
+	//inizialize the map
 	var map = new google.maps.Map(document.getElementById('google-container'), map_options);
 	//add a custom marker to the map				
 	var marker = new google.maps.Marker({
-	  	position: new google.maps.LatLng(latitude, longitude),
-	    map: map,
-	    visible: true,
-	 	icon: marker_url,
+		position: new google.maps.LatLng(latitude, longitude),
+		map: map,
+		visible: true,
+		icon: marker_url,
 	});
 
 	//add custom buttons for the zoom-in/zoom-out on the map
 	function CustomZoomControl(controlDiv, map) {
 		//grap the zoom elements from the DOM and insert them in the map 
-	  	var controlUIzoomIn= document.getElementById('cd-zoom-in'),
-	  		controlUIzoomOut= document.getElementById('cd-zoom-out');
-	  	controlDiv.appendChild(controlUIzoomIn);
-	  	controlDiv.appendChild(controlUIzoomOut);
+		var controlUIzoomIn = document.getElementById('cd-zoom-in'),
+			controlUIzoomOut = document.getElementById('cd-zoom-out');
+		controlDiv.appendChild(controlUIzoomIn);
+		controlDiv.appendChild(controlUIzoomOut);
 
 		// Setup the click event listeners and zoom-in or out according to the clicked element
-		google.maps.event.addDomListener(controlUIzoomIn, 'click', function() {
-		    map.setZoom(map.getZoom()+1)
+		google.maps.event.addDomListener(controlUIzoomIn, 'click', function () {
+			map.setZoom(map.getZoom() + 1)
 		});
-		google.maps.event.addDomListener(controlUIzoomOut, 'click', function() {
-		    map.setZoom(map.getZoom()-1)
+		google.maps.event.addDomListener(controlUIzoomOut, 'click', function () {
+			map.setZoom(map.getZoom() - 1)
 		});
 	}
 
 	var zoomControlDiv = document.createElement('div');
- 	var zoomControl = new CustomZoomControl(zoomControlDiv, map);
+	var zoomControl = new CustomZoomControl(zoomControlDiv, map);
 
-  	//insert the zoom div on the top left of the map
-  	map.controls[google.maps.ControlPosition.LEFT_TOP].push(zoomControlDiv);
+	//insert the zoom div on the top left of the map
+	map.controls[google.maps.ControlPosition.LEFT_TOP].push(zoomControlDiv);
 });
-
-  
